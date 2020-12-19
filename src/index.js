d1a5ed6c8ba147468d524d234c08bacaa8f1e954
valeria_gora <valeriia.gora@jmind.systems>
2020-12-19 19:22:48 +0200

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css';
+
+/* eslint-disable react/jsx-filename-extension */
+const App = () => <div>App</div>;
+
+/* eslint-disable no-undef */
+ReactDOM.render(<App />, document.querySelector('#root'));
